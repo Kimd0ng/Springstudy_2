3bcd62cef7da7151f559bbb233d642f83b276a27
Kimd0ng <kdongh0406@gmail.com>
2024-07-05 20:49:59 +0900

    feat: 2주차 과제 해결

--- src/main/java/com/example/springstudy/repository/OrderItemRepository.java
+++ src/main/java/com/example/springstudy/repository/OrderItemRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     List<OrderItem> findByOrderId(Orders orderId);  //List를 활용하기 위해 Orders 타입으로 넣어야됨
+    //처음에 orderid를 이용해 select를 진행했는데 이렇게 하니까 order를 이용해서 하라고 나옴
 }
